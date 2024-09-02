3c262d26433554e758aba9d24ee713488f451337
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-09-02 12:00:36 -0500

    Updated Base URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   url: "https://blog.brycemcwhirter.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/my-blog/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
