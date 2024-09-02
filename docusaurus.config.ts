9e51695ef2cb96ccb91a751a9fb091243d9695df
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-09-02 11:38:01 -0500

    update project name and trailing slash

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,8 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "brycemcwhirter", // Usually your GitHub org/user name.
-  projectName: "my-blog", // Usually your repo name.
+  projectName: "brycemcwhirter.github.io", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
